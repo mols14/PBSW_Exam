b10dea44b8c1d801be7977416342964e3c6360b4
mols14 <71692370+mols14@users.noreply.github.com>
2024-05-17 22:17:06 +0200

    init

---
+++ PBSW_Exam_GUI/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace PBSW_Exam_GUI;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
